9bf98c6bd27ae2e25df441492b96bbd1f2ccfd34
alex-min <nobody@example.com>
2013-06-02 19:13:54 +0100

    commit of the code documentation (generated with yuidoc)

---
+++ documentation/code/api.js
@@ -0,0 +1,11 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Jelly",
+        "Logger",
+        "ReadableEntity"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
